503a001ce55ecd974ac0e82b2c1c4543fbe58551
Sravan Konda <sravan.konda@ibm.com>
2022-06-06 12:30:21 +0530

    students are future of the country to at school

---
+++ StudTeacher.java
@@ -0,0 +1,19 @@
+public class StudTeacher
+{
+  public void Teacher(){
+    
+       System.out.println("teaching is the profession that teaches all the other professions");
+       System.out.println("teacher is equal to god");
+  }
+   public void Student(){
+       
+       System.out.println("at school");
+  }
+   public static void main(String args[])
+  {
+     StudTeacher t=new StudTeacher();
+     t.Teacher();
+     t.Student();
+  }
+}
+
